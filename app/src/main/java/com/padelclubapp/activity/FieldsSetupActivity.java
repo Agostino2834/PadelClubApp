3fe47be448380293d3c0fdb7c860b0529b7666f0
Agostino Isgrò <agostino.isgro@external.nexigroup.com>
2024-07-08 21:33:35 +0200

    implementing setUp data app

--- app/src/main/java/com/padelclubapp/activity/FieldsSetupActivity.java
+++ app/src/main/java/com/padelclubapp/activity/FieldsSetupActivity.java
@@ -3,34 +3,61 @@ package com.padelclubapp.activity;
 import android.os.Bundle;
 import android.view.View;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 import com.padelclubapp.adapter.FieldsAdapter;
 import com.padelclubapp.databinding.ActivityFieldsSetupBinding;
 import com.padelclubapp.dataclass.Campi;
+import com.padelclubapp.dataclass.Users;
 
 import java.util.ArrayList;
 
 public class FieldsSetupActivity extends AppCompatActivity {
     private ActivityFieldsSetupBinding binding;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        binding= ActivityFieldsSetupBinding.inflate(getLayoutInflater());
+        binding = ActivityFieldsSetupBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
         setUpToolbar();
-        binding.recycle.setLayoutManager(new LinearLayoutManager(this));
+        setAdapter();
 
-        ArrayList<Campi> padelCourtList = new ArrayList<>();
-        // Aggiungi alcuni dati alla lista
-        padelCourtList.add(new Campi("Campo 1", "Esteriore", true,2));
-        padelCourtList.add(new Campi("Campo 2", "Interiore", true, 2));
-        // Aggiungi altri campi se necessario
-
-        FieldsAdapter adapter = new FieldsAdapter(padelCourtList, this);
-        binding.recycle.setAdapter(adapter);
     }
+
+    private void setAdapter() {
+        DatabaseReference mDatabase = FirebaseDatabase.getInstance("https://padelclubapp-default-rtdb.firebaseio.com").getReference("campi");
+        binding.recycle.setLayoutManager(new LinearLayoutManager(this));
+        ArrayList<Campi> campi = new ArrayList<>();
+        FieldsAdapter adapter = new FieldsAdapter(campi, this);
+        binding.recycle.setAdapter(adapter);
+        mDatabase.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                for (DataSnapshot userSnapshot : snapshot.getChildren()) {
+                    campi.add(userSnapshot.getValue(Campi.class));
+                    adapter.notifyDataSetChanged();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+    }
+
     private void setUpToolbar() {
         binding.toolbar.backIcon.setVisibility(View.GONE);
         binding.toolbar.toolbarTitle.setText("Personalizza la tua app");
